959f2cb9dfa8b8070588f2b736502abf01b025c4
Raymond Dokholyan <58710772+urayed@users.noreply.github.com>
2026-07-01 17:40:38 -0400

    Redone firmware
    now in uf2 format

---
+++ macropad9/keymaps/default/keymap.c
@@ -0,0 +1,21 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ A │ B │ C │
+     * ├───┼───┼───┤
+     * │ D │ E │ F │
+     * ├───┼───┼───┤
+     * │ G │ H │ I │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_3x3(
+        KC_COPY,    KC_B,    KC_C,
+        KC_D,    KC_E,    KC_F,
+        KC_G,    KC_H,    KC_I
+    )
+};
